135a7703104aecb85c29cfb316d1cb0035747e64
ThiagoAtaide17 <thiagoataidegoncalves@gmail.com>
2024-09-05 23:15:46 -0300

    Desafio Concluido!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // TODO: IMPLEMENTADO!!
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+         string App { get; set; }
+        // TODO: IMPLEMENTADO!!
+        public override void InstalarAplicativo(string nome)
+        {
+            App = nome;
+            Console.WriteLine($"Instalando o aplicativo '{App}' no Iphone");
+            
+        }
     }
 }
